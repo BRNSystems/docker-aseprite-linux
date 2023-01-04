727f3fcb107173330e197c0cf56b9ab0d503a987
Bruno Rybársky <bruno@brn.systems>
2023-01-04 10:17:02 +0100

    upgrade skia

--- compile.sh
+++ compile.sh
@@ -13,7 +13,7 @@ fi
 
 if [ ! -d "/dependencies/skia" ]
 then
-  git clone -b aseprite-m96 https://github.com/aseprite/skia.git
+  git clone -b aseprite-m102 https://github.com/aseprite/skia.git
 fi
 
 export PATH="${PWD}/depot_tools:${PATH}"
